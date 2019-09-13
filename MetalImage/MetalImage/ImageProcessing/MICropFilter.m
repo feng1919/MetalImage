47123218f782314ef941ba09db6c07995d742f22
Feng Stone <redpark@qq.com>
2019-09-13 18:31:25 +0800

    adport for rotation

--- MetalImage/MetalImage/ImageProcessing/MICropFilter.m
+++ MetalImage/MetalImage/ImageProcessing/MICropFilter.m
@@ -43,56 +43,56 @@
     
     switch(firstInputParameter.rotationMode)
     {
-        case kMetalImageNoRotation: // Works
+        case kMetalImageNoRotation:
         {//{A,B,C,D}
             cropTextureCoordinates[0] = MTLFloat2Make(minX, maxY);
             cropTextureCoordinates[1] = MTLFloat2Make(maxX, maxY);
             cropTextureCoordinates[2] = MTLFloat2Make(minX, minY);
             cropTextureCoordinates[3] = MTLFloat2Make(maxX, minY);
         }; break;
-        case kMetalImageRotateLeft: // Fixed
+        case kMetalImageRotateLeft:
         {//{C,A,D,B}
             cropTextureCoordinates[0] = MTLFloat2Make(minY, 1.0-maxX);
             cropTextureCoordinates[1] = MTLFloat2Make(minY, 1.0-minX);
             cropTextureCoordinates[2] = MTLFloat2Make(maxY, 1.0-maxX);
             cropTextureCoordinates[3] = MTLFloat2Make(maxY, 1.0-minX);
         }; break;
-        case kMetalImageRotateRight: // Fixed
+        case kMetalImageRotateRight:
         {//{B,D,A,C}
             cropTextureCoordinates[0] = MTLFloat2Make(maxY, 1.0-minX);
             cropTextureCoordinates[1] = MTLFloat2Make(maxY, 1.0-maxX);
             cropTextureCoordinates[2] = MTLFloat2Make(minY, 1.0-minX);
             cropTextureCoordinates[3] = MTLFloat2Make(minY, 1.0-maxX);
         }; break;
-        case kMetalImageFlipVertical: // Works for me
+        case kMetalImageFlipVertical:
         {//{C,D,A,B}
             cropTextureCoordinates[0] = MTLFloat2Make(minX, minY);
             cropTextureCoordinates[1] = MTLFloat2Make(maxX, minY);
             cropTextureCoordinates[2] = MTLFloat2Make(minX, maxY);
             cropTextureCoordinates[3] = MTLFloat2Make(maxX, maxY);
         }; break;
-        case kMetalImageFlipHorizonal: // Works for me
+        case kMetalImageFlipHorizonal:
         {//{B,A,D,C}
             cropTextureCoordinates[0] = MTLFloat2Make(maxX, maxY);
             cropTextureCoordinates[1] = MTLFloat2Make(minX, maxY);
             cropTextureCoordinates[2] = MTLFloat2Make(maxX, minY);
             cropTextureCoordinates[3] = MTLFloat2Make(minX, minY);
         }; break;
-        case kMetalImageRotate180: // Fixed
+        case kMetalImageRotate180:
         {//{D,C,B,A}
             cropTextureCoordinates[0] = MTLFloat2Make(maxX, minY);
             cropTextureCoordinates[1] = MTLFloat2Make(minX, minY);
             cropTextureCoordinates[2] = MTLFloat2Make(maxX, maxY);
             cropTextureCoordinates[3] = MTLFloat2Make(minX, maxY);
         }; break;
-        case kMetalImageRotateRightFlipVertical: // Fixed
+        case kMetalImageRotateRightFlipVertical:
         {//{D,B,C,A}
             cropTextureCoordinates[0] = MTLFloat2Make(maxY, 1.0-maxX);
             cropTextureCoordinates[1] = MTLFloat2Make(maxY, 1.0-minX);
             cropTextureCoordinates[2] = MTLFloat2Make(minY, 1.0-maxX);
             cropTextureCoordinates[3] = MTLFloat2Make(minY, 1.0-minX);
         }; break;
-        case kMetalImageRotateRightFlipHorizontal: // Fixed
+        case kMetalImageRotateRightFlipHorizontal:
         {//{A,C,B,D}
             cropTextureCoordinates[0] = MTLFloat2Make(minY, 1.0-minX);
             cropTextureCoordinates[1] = MTLFloat2Make(minY, 1.0-maxX);
@@ -111,13 +111,22 @@ MTLFloat Distance(MTLFloat2 *p0, MTLFloat2 *p1)
 
 - (MTLUInt2)textureSizeForOutput {
     MTLUInt2 textureSize = [super textureSizeForOutput];
+    MTLFloat tx, ty;
+    if (MetalImageRotationSwapsWidthAndHeight(firstInputParameter.rotationMode)) {
+        tx = textureSize.y;
+        ty = textureSize.x;
+    }
+    else {
+        tx = textureSize.x;
+        ty = textureSize.y;
+    }
     MTLFloat2 A, B, C;
-    A.x = cropTextureCoordinates[0].x * textureSize.x;
-    A.y = cropTextureCoordinates[0].y * textureSize.y;
-    B.x = cropTextureCoordinates[1].x * textureSize.x;
-    B.y = cropTextureCoordinates[1].y * textureSize.y;
-    C.x = cropTextureCoordinates[2].x * textureSize.x;
-    C.y = cropTextureCoordinates[2].y * textureSize.y;
+    A.x = cropTextureCoordinates[0].x * tx;
+    A.y = cropTextureCoordinates[0].y * ty;
+    B.x = cropTextureCoordinates[1].x * tx;
+    B.y = cropTextureCoordinates[1].y * ty;
+    C.x = cropTextureCoordinates[2].x * tx;
+    C.y = cropTextureCoordinates[2].y * ty;
     MTLFloat width = Distance(&A, &B);
     MTLFloat height = Distance(&C, &A);
     return MTLUInt2Make(roundf(width), roundf(height));
@@ -128,10 +137,10 @@ MTLFloat Distance(MTLFloat2 *p0, MTLFloat2 *p1)
 
 - (void)setCropRegion:(CGRect)newValue;
 {
-//    NSParameterAssert(newValue.origin.x >= 0 && newValue.origin.x <= 1 &&
-//                      newValue.origin.y >= 0 && newValue.origin.y <= 1 &&
-//                      newValue.size.width >= 0 && newValue.size.width <= 1 &&
-//                      newValue.size.height >= 0 && newValue.size.height <= 1);
+    //    NSParameterAssert(newValue.origin.x >= 0 && newValue.origin.x <= 1 &&
+    //                      newValue.origin.y >= 0 && newValue.origin.y <= 1 &&
+    //                      newValue.size.width >= 0 && newValue.size.width <= 1 &&
+    //                      newValue.size.height >= 0 && newValue.size.height <= 1);
     
     _cropRegion = newValue;
     [self calculateCropTextureCoordinates];
@@ -140,7 +149,7 @@ MTLFloat Distance(MTLFloat2 *p0, MTLFloat2 *p1)
 - (void)setInputRotation:(MetalImageRotationMode)newInputRotation atIndex:(NSInteger)textureIndex;
 {
     BOOL needCalculateCropRegion = newInputRotation != firstInputParameter.rotationMode;
-
+    
     [super setInputRotation:newInputRotation atIndex:textureIndex];
     
     if (needCalculateCropRegion) {
