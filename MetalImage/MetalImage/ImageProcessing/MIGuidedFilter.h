cf55ae5f353ccd54d5d4609e9b5b0ebcbdbe2f01
Feng Stone <redpark@qq.com>
2019-11-07 19:50:51 +0800

    add guided filter

---
+++ MetalImage/MetalImage/ImageProcessing/MIGuidedFilter.h
@@ -0,0 +1,17 @@
+//
+//  MIGuidedFilter.h
+//  MetalImage
+//
+//  Created by Feng Stone on 2019/11/7.
+//  Copyright © 2019 fengshi. All rights reserved.
+//
+
+#import <MetalImage/MetalImage.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MIGuidedFilter : MetalImageFilter
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MetalImage/MetalImage/ImageProcessing/MIGuidedFilter.m
@@ -0,0 +1,13 @@
+//
+//  MIGuidedFilter.m
+//  MetalImage
+//
+//  Created by Feng Stone on 2019/11/7.
+//  Copyright © 2019 fengshi. All rights reserved.
+//
+
+#import "MIGuidedFilter.h"
+
+@implementation MIGuidedFilter
+
+@end
